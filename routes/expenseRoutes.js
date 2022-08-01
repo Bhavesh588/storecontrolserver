393e4adb5189e08580f687fa45738033117623c6
ayelen <ayee_01@live.com>
2022-08-01 20:35:48 -0300

    route edit

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -9,5 +9,19 @@ router.get("/", (req, res)=>{
 router.post("/new", (req, res)=>{
     db.Expense_master.create(req.body).then((submittedExpense)=> res.send(submittedExpense))
 })
+router.put("/edit", (req, res) => {
+    db.Expense_master.update(req.body, {
+        where: {
+            ExpenseId: req.body.ExpenseId,
+        },
+    }).then(() => res.send("successfully Updated"));
+});
+router.delete("/delete/:id", (req, res) => {
+    db.Expense_master.destroy({
+        where: {
+          ExpenseId: req.params.id,
+        },
+    }).then(() => res.send("success"));
+});
 
 module.exports = router;
